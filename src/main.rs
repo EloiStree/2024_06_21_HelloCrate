eff20165820d51bc1063a09d8969375feea7f8a1
Eloi Stree <eloistree@gmail.com>
2024-06-21 21:52:45 +0200

    save

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,11 @@ use be_eloistree_hellocrate::test_gamepad_to_integer;
 /// test the code from maint
 fn main() {
     
+    // Lol :)
+    // Created my first crate:
+    // https://crates.io/crates/be_eloistree_hellocrate
+    // Big thanks to this guy :) 
+    // https://youtu.be/4TI153PIEDQ
     test_gamepad_to_integer();
 }
 
